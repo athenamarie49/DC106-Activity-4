698a79f2aadc0e5f7e8f954f1aaaf041b1449839
athenamarie49 <athenamariee49@gmail.com>
2021-09-28 23:52:22 +0800

    first commit

---
+++ MidtermAct/src/app/participants/participants.service.ts
@@ -0,0 +1,17 @@
+export class ParticipantsService {
+    getParticipants(){
+        return [
+            {name:"Joel Guinea", age:"20", gender:"Male"},
+            {name:"Athena Marie Alombro", age:"21", gender:"Female"},
+            {name:"Alvin Jose Orcullo", age:"22", gender:"Male"},
+            {name:"Lyca Tocmo", age:"21", gender:"Female"},
+            {name:"Clear Petalino", age:"22", gender:"Female"},
+            {name:"Juacinto Borgo", age:"21", gender:"Male"},
+            {name:"Malicia Ulan", age:"19", gender:"Female"},
+            {name:"Loloy Mo", age:"20", gender:"Male"},
+            {name:"ALicia Si", age:"21", gender:"Female"},
+            {name:"Por To", age:"21", gender:"Male"},
+            {name:"La Toc", age:"25", gender:"Female"},
+          ];
+    }
+}
